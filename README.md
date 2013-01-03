cbb5f9c7f3882e48d1e253501df9a05ca51e6877
Fokke Zandbergen <mail@fokkezandbergen.nl>
2013-01-03 20:09:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ganttoday
+This is a simpel extension for the [Gantter project](http://gantter.com). It displays today's tasks for your Gantt projects. Gantter is a FREE web-based project management tool. You can think of it as a web-based Microsoft Project.
+
+More information coming soon.
---
+++ gantter.php
@@ -0,0 +1,49 @@
+<?php 
+
+$projectXML = simplexml_load_string($_POST["projectXML"]);
+
+$sxe = new SimpleXMLElement($projectXML->asXML());
+$sxe->registerXPathNamespace('n', 'http://schemas.microsoft.com/project');
+
+$tasks = $sxe->xpath('/n:Project/n:Tasks/n:Task[n:Summary = 0 and n:Start[starts-with(text(), "' . date('Y-m-d') . '")]]');
+
+header('Content-type: text/html');
+
+/* TODO:
+
+	- Instructie om project naar vandaag op te schuiven
+	- Pad naar getoonde taken laten zien (via hiërarchie, niet via afhankelijkheden)
+	- Percentage voltooid wijzigen (ook dmv checkbox op 0% of 100%)
+	
+	- Opmaak
+	- Blog
+	
+	- Eventueel notities nog direct kunnen wijzigen
+
+ */
+
+?> 
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		<title>Gantter.com - Today</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7">
+		<meta name="viewport" content="width=device-width,height=device-height,maximum-scale=1.0">
+		<link rel="stylesheet" href="https://app.gantter.com/css/styles.css?v=15242168" type="text/css" media="screen,projection,print">
+		<link rel="stylesheet" href="https://app.gantter.com/css/default-theme.css?v=15242168" type="text/css" media="screen,projection,print">
+		<link rel="icon" href="https://app.gantter.com/images/favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" href="https://app.gantter.com/images/favicon.ico" type="image/x-icon">
+		<link href="https://www.google.com/uds/api/picker/1.0/05c87704cd84b49307c16b1e4e9aee7c/default.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+	
+		<h1>Today</h1>
+
+		<ul>
+			<? foreach ($tasks as $task): ?>
+				<li><?= $task->Name ?></li>
+			<? endforeach ?>
+		</ul>
+	
+	</body>
+</html>
